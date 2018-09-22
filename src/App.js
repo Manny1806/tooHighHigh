e57df80c70542ef64900fedc5ad6f5008dc311e5
manny1806 <manny1806@gmail.com>
2018-09-21 19:34:11 -0700

    buttons

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      page: "landing"
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">So you got too high?</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          That's ok. That's why we are here! Just select a button below that corresponds to the person/thing you are trying to talk to.</p>
+        <div className="button-group">
+          <button className="talk-button">Parents</button>
+          <button className="talk-button">Pizza Guy</button>
+          <button className="talk-button">Partner</button>
+          <button className="talk-button">COPS!</button>
+          <button className="talk-button">Parents Again</button>
+        </div>
       </div>
     );
   }
